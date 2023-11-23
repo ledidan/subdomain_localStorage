6a8402d206e818c2f2dc842c72f02798cb5ac93d
Bon <didan.mobe@gmail.com>
2023-11-23 09:00:26 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -19,22 +19,13 @@ class App extends React.Component {
     window.removeEventListener("message", this.handleMessage);
   }
   handleMessage = (event) => {
-    // Validate the origin of the message for security reasons
     const subdomainFrame = document.getElementById("subdomain-frame");
     // Post a message to the subdomain
-    subdomainFrame.contentWindow.postMessage("https://www.skiplisalon.com");
-    if (event.origin === "https://www.skiplisalon.com") {
-      console.log("Received message from root domain:", event.data);
-      console.log("Received message from root domain uuid:", event.data.uuid);
-
-      // Handle the data received from the root domain
-      const rootDomainUUID = event.data.uuid;
-
-      // Save the root domain UUID to local storage
-      localStorage.setItem("rootDomainUUID", rootDomainUUID);
-
-      // Optionally, you can update the state if needed
-      this.setState({ uuid: rootDomainUUID });
+    subdomainFrame.contentWindow.postMessage(
+      "https://www.skiplisalon.com"
+    );
+    if (event?.data) {
+      this.setState({ uuid: event.data });
     }
   };
 
@@ -45,10 +36,9 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>SUB DOMAIN SKIPLI LOCAL STORAGE</h1>
-          <p>UUID: {uuid}</p>
+          <p>UUID: {uuid.length > 0 ? uuid : "No message from root domain"}</p>
         </header>
         <iframe
-          // style={{ display: "none" }}
           id="subdomain-frame"
           title="Subdomain Frame"
           src="https://www.skiplisalon.com"
